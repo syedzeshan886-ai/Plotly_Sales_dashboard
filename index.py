2b9ce340682ee8b18f218da2752835bbf7c7c673
Zeshan Bukhari <zeshan.bukhari@pk.igate.com.pk>
2026-09-17 19:03:19 +0500

    debug true

--- index.py
+++ index.py
@@ -82,4 +82,4 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='127.0.0.1', port=8051)
+    app.run_server(debug=False, host='127.0.0.1', port=8051)
